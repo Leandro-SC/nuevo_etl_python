32740393b857b9808a9ac31c1bd431cfc8c9e9c6
LeitoSilvaC <65784692+Leandro-SC@users.noreply.github.com>
2023-01-16 18:48:49 -0500

    Update Class_BD.py

--- models/Class_BD.py
+++ models/Class_BD.py
@@ -11,26 +11,7 @@ bd = str(os.getenv('BD'))
 usuario = str(os.getenv('USUARIO'))
 contrasena = str(os.getenv('PASSWORD'))
 query = '''
-  SELECT 
-		COUNT(peticion) as venta, MONTH([fecha_registro]) as mes, [documento_vendedor], DAY( fecha_registro) as dia, [zonal_vendedor], YEAR([fecha_registro]) as year,
-  	CASE 
-		WHEN DATEPART(WEEKDAY,fecha_registro) = 1 THEN 'lunes'
-		WHEN DATEPART(WEEKDAY,fecha_registro) = 2 THEN 'martes'
-		WHEN DATEPART(WEEKDAY,fecha_registro) = 3 THEN 'miercoles'
-		WHEN DATEPART(WEEKDAY,fecha_registro) = 4 THEN 'jueves'
-		WHEN DATEPART(WEEKDAY,fecha_registro) = 5 THEN 'viernes'
-		WHEN DATEPART(WEEKDAY,fecha_registro) = 6 THEN 'sabado'
-		WHEN DATEPART(WEEKDAY,fecha_registro) = 7 THEN 'domingo'
-		ELSE 'DIA NO IDENTIFICADO'
-	END AS DiaSemana
-  FROM 
-		[pbi].[dbo].[FJ_venta_registros] 
-  WHERE 
-		YEAR([fecha_registro])>=2022 AND [documento_vendedor] <> 'CTG000009'
-  GROUP BY 
-		[zonal_vendedor], [documento_vendedor], MONTH([fecha_registro]), DAY( fecha_registro), DATEPART(WEEKDAY,fecha_registro), YEAR([fecha_registro]) 
-  ORDER BY 
-		MONTH([fecha_registro]), DAY( fecha_registro), YEAR([fecha_registro]);
+AGREGAR QUERY
     '''
 
 
