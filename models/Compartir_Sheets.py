de34811c6484c4b19f6aef535b00540eea97d579
LeitoSilvaC <65784692+Leandro-SC@users.noreply.github.com>
2023-01-16 18:50:15 -0500

    Update Compartir_Sheets.py

--- models/Compartir_Sheets.py
+++ models/Compartir_Sheets.py
@@ -14,7 +14,7 @@ cliente = gspread.authorize(credenciales)
 # sheet.share('leandrosilva.auren@gmail.com', perm_type='user', role='writer', notify=True)
 def actualizarHoja(archivoExcel):
 
-    libro_lectura = cliente.open_by_key("1C4fkxWrXavRs6Vx0l5ejbOeXPy3k-98ssQbufUDVCHw").sheet1
+    libro_lectura = cliente.open_by_key("CLAVE_SHEETS").sheet1
     df = pd.read_excel(archivoExcel)
     datos_columnas = df.columns.values.tolist()
     datos_valores = df.values.tolist()
